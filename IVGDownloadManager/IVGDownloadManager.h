9c399ab0db770b7fdbfdde871713597c7fdd81fe
Doug Sjoquist <dwsjoquist@sunetos.com>
2012-01-29 22:10:07 -0500

    Initial Commit

---
+++ IVGDownloadManager/IVGDownloadManager.h
@@ -0,0 +1,13 @@
+//
+//  IVGDownloadManager.h
+//  IVGDownloadManager
+//
+//  Created by Sjoquist Douglas on 1/29/12.
+//  Copyright (c) 2012 Ivy Gulch, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IVGDownloadManager : NSObject
+
+@end
---
+++ IVGDownloadManager/IVGDownloadManager.m
@@ -0,0 +1,23 @@
+//
+//  IVGDownloadManager.m
+//  IVGDownloadManager
+//
+//  Created by Sjoquist Douglas on 1/29/12.
+//  Copyright (c) 2012 Ivy Gulch, LLC. All rights reserved.
+//
+
+#import "IVGDownloadManager.h"
+
+@implementation IVGDownloadManager
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
